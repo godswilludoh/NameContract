f8ecaab189372cb34cd8ae3714dea03448b44380
Godswill Udoh <godswilludoh@gmail.com>
2022-06-26 21:08:24 +0100

    created a simple name store and retrieve contract

---
+++ name.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+contract MyName {
+    //initializing to 0
+    string name;
+
+    //adding a store function
+    function store(string memory _name) public{
+        name = _name;
+    }
+
+    //adding a retrive function
+    function retrieve() public view returns(string){
+        return name;
+    }
+  
+}

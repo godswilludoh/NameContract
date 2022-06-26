6edd12dffc767ba3bc58f06ad114b6e86c642c91
Godswill Udoh <godswilludoh@gmail.com>
2022-06-26 21:21:44 +0100

    made changes to the retrieve function

--- name.sol
+++ name.sol
@@ -6,13 +6,12 @@ contract MyName {
     string name;
 
     //adding a store function
-    function store(string memory _name) public{
+    function store(string memory _name) public {
         name = _name;
     }
 
     //adding a retrive function
-    function retrieve() public view returns(string){
+    function retrieve() public view returns (string memory) {
         return name;
     }
-  
 }
